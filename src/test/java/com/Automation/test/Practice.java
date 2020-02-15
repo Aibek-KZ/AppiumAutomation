300f0cd02347ee4651d28f9551880cfc370f5abd
Aibek-KZ <aibek.risbek@gmail.com>
2020-02-14 17:30:42 -0700

    first commit

---
+++ src/test/java/com/Automation/test/Practice.java
@@ -0,0 +1,22 @@
+package com.Automation.test;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.Test;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.remote.DesiredCapabilities;
+//
+//public class Practice {
+//
+//    AppiumDriver<MobileElement> driver;
+//
+//    @Test
+//    public void test1(){
+//        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+//        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
+//        desiredCapabilities.setCapability(MobileCapabilityType.VERSION,"7.0");
+//        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"Pixel_2");
+//        desiredCapabilities.setCapability(MobileCapabilityType);
+//    }
+//}
